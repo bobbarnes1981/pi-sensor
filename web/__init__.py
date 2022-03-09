949a341739742048688e66b47a48d91163c08ffd
Robert Barnes <robertbarnes1981@googlemail.com>
2022-03-09 15:30:13 +0000

    Draw graph

--- web/__init__.py
+++ web/__init__.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
+import time
 
 db_file = 'sqlite.db'
 app = Flask(__name__)
@@ -9,11 +10,17 @@ app = Flask(__name__)
 def index():
     con = sqlite3.connect(db_file)
     cur = con.cursor()
-    cur.execute('SELECT timestamp, office, outside FROM readings ORDER BY timestamp DESC limit 1')
-    rec = cur.fetchone()
-    t = rec[0]
-    d = datetime.fromtimestamp(t, timezone.utc)
-    office = rec[1]
-    outside = rec[2]
+    cur.execute('SELECT timestamp, office, outside FROM readings WHERE timestamp > ? ORDER BY timestamp ASC', (time.time() - 60*60*24, ))
+    recs = cur.fetchall()
+    data = []
+    skip = 10
+    count = 0
+    for rec in recs:
+        if count == 0:
+            data.append((datetime.fromtimestamp(rec[0], timezone.utc), rec[1], rec[2]))
+        count = count + 1
+        if count > skip:
+            count = 0
     con.close()
-    return f"<p>Timestamp: {d} ({t}) Office: {office} Outside: {outside}</p>"
+    #return f"<p>Timestamp: {d} ({t}) Office: {office} Outside: {outside}</p>"
+    return render_template('index.html', recs=data)
